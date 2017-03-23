7b18f7026d010e8b28039115952f727b4439987c
SHashank <shash.143.kumar@gmail.com>
2017-03-24 03:55:09 +0530

    New

---
+++ js/sectiontoggle.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(".greenact").click(function(){
+		$(".testimonials").toggle();
+		$(".orgclient").hide();
+		$(".notable").hide();
+	});
+	$(".purpleact").click(function(){
+		$(".orgclient").toggle();
+		$(".testimonials").hide();
+		$(".notable").hide();
+	});
+	$(".redact").click(function(){
+		$(".notable").toggle();
+		$(".testimonials").hide();
+		$(".orgclient").hide();
+	});
+});
